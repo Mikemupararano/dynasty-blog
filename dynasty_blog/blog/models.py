f5189dad1089bf67cce96bffe3b86867c3b73295
Mike Mupararano <kudath@yahoo.co.uk>
2025-10-18 13:57:14 +0100

    Added a status field to models.py to show whether a post is publisged or a draft.

--- dynasty_blog/blog/models.py
+++ dynasty_blog/blog/models.py
@@ -4,6 +4,11 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+    # Adding a status field to indicate whether a blog post is a draft or published
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
@@ -11,15 +16,19 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT,
+    )
 
-    #Present blogs in reverse chronological order, showing the most recent posts first
+    # Present blogs in reverse chronological order, showing the most recent posts first
     class Meta:
-        ordering = ['-published']
-        #Adding an index on the published field to optimize queries that filter or sort by this field
+        ordering = ["-published"]
+        # Adding an index on the published field to optimize queries that filter or sort by this field
         indexes = [
-            models.Index(fields=['-published']),
+            models.Index(fields=["-published"]),
         ]
-
 
     def __str__(self):
         return self.title
