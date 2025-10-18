a4f119788c78dcdef23a3174b39a63ca491d0b8d
Mike Mupararano <kudath@yahoo.co.uk>
2025-10-18 19:48:16 +0100

    Created a view to display a single post.

--- dynasty_blog/blog/views.py
+++ dynasty_blog/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, "blog/post/list.html", {"posts": posts})
+
+
+# Create a view to display a single post
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+    return render(request, "blog/post/detail.html", {"post": post})
