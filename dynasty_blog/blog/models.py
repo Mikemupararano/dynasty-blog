022fe10ee67710d69dca98df90b92ab977d0ad1d
Mike Mupararano <kudath@yahoo.co.uk>
2025-10-18 13:46:53 +0100

    Edited models.py.

--- dynasty_blog/blog/models.py
+++ dynasty_blog/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
