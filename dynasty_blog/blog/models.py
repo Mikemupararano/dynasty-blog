ae3758ebc6ed81cc67f1aa106abd55507052b4d2
Mike Mupararano <kudath@yahoo.co.uk>
2025-10-18 13:51:01 +0100

    Added a databse index to models.py.

--- dynasty_blog/blog/models.py
+++ dynasty_blog/blog/models.py
@@ -12,5 +12,14 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    #Present blogs in reverse chronological order, showing the most recent posts first
+    class Meta:
+        ordering = ['-published']
+        #Adding an index on the published field to optimize queries that filter or sort by this field
+        indexes = [
+            models.Index(fields=['-published']),
+        ]
+
+
     def __str__(self):
         return self.title
