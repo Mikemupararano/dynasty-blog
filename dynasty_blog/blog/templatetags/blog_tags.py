c035e513fad40a730949a3263de838776679fc8c
Mike Mupararano <kudath@yahoo.co.uk>
2025-10-21 22:11:29 +0100

    Created an inclusion template tag.

---
+++ dynasty_blog/blog/templatetags/blog_tags.py
@@ -0,0 +1,19 @@
+# blog/templatetags/blog_tags.py
+from django import template
+from blog.models import Post
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts():
+    """Returns the total number of published blog posts."""
+    return Post.published_posts.count()
+
+
+# Creating an inclusion template tag
+@register.inclusion_tag("blog/post/latest_posts.html")
+def show_latest_posts(count=5):
+    """Returns the latest published blog posts."""
+    latest_posts = Post.published_posts.order_by("-publish")[:count]
+    return {"latest_posts": latest_posts}
